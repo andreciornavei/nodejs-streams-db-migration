1340730f8845a77ccbab2eccd1fafecfea4a0727
André Ciornavei <andreciornavei@MacBook-Pro-de-Andre.local>
2022-10-27 18:49:05 -0300

    implemented basic pipeline to stream sql data into an output.txt file

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import _ from "lodash";
+import knex from "knex";
+import { Transform } from "stream";
+
+// <!-- creat an instance to source database -->
+const sourceDatabase = knex({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "admin",
+    database: "source_database",
+  },
+});
+
+// <!-- execute a query stream to start receiving data -->
+const readtableStream = sourceDatabase
+  .select(["id", "name", "email"])
+  .from("bulk_table")
+  .stream();
+
+// <!-- create a function to transform data from source pattern to target pattern
+const dataTransformer = new Transform({
+  writableObjectMode: true,
+  transform(chunk, _encoding, callback) {
+    callback(
+      null,
+      [_.get(chunk, "id"), _.get(chunk, "name"), _.get(chunk, "email")].join(
+        "|"
+      )
+    );
+  },
+});
+
+// <!-- create a function to print data transformation
+const dataPrinter = new Transform({
+  writableObjectMode: true,
+  transform(chunk, _encoding, callback) {
+    console.log(String(chunk));
+    callback(null, chunk);
+  },
+});
+
+// <!-- create a writable scream to srite output data into a file
+const writableStream = fs.createWriteStream("output.txt");
+
+// <!-- pipe ETL streams on readable stream
+readtableStream.pipe(dataTransformer).pipe(dataPrinter).pipe(writableStream);
+readtableStream.on("end", () => console.log("finished"));
